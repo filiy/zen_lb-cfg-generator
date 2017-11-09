3b2d9ebe655d0acfa2b3e2cdf77a4e997b600d68
Phil Thomson <thomson.p@gmail.com>
2017-11-09 10:04:45 -0800

    adding in files

---
+++ zen_lb-cfg-generator.ps1
@@ -0,0 +1,34 @@
+﻿#this script will generate .cfg files for zen load balancer which can be used to create "farms"
+#this script will take input from a csv file(example included with script) and generate cfg files
+#based on the template cfg file. The script was used for create vips or farms with 2 servers in the pool
+#but could be modified to do 1 server in the pool. Once the cfg are generated you can copy them over to
+#/usr/local/zenloadbalancer/config on the zenloadbalancer server and then restart the zenloadbalancer service 
+#and the farms should be created. You will need to create the VIP interfaces before you restart the service
+#
+#
+#pthomson - April 9, 2015
+#
+#
+#NAME = name of the VIP
+#LOCALPORT = random number for the 127.0.0.1 local port. Still not 100% how this is used 
+#VIP = VIP IP address
+#SRCPRT = VIP port number 
+#DESTIP = IP address of the server in the vip pool
+#DESTPORT = port on the destination server in the vip pool
+
+
+
+$csv = Read-host "Enter the name of the .CSV file located in the same folder as this script"
+$vips = Import-Csv $csv
+
+foreach ($vip in $vips) {
+    
+    (Get-Content .\template.cfg) -replace "NAME",$vip.Name | Set-Content "$($vip.name)_pen.cfg"
+    (Get-Content "$($vip.name)_pen.cfg") -replace "LOCALPORT",$vip.lport | Set-Content "$($vip.name)_pen.cfg"
+    (Get-Content "$($vip.name)_pen.cfg") -replace "VIP",$vip.vip | Set-Content "$($vip.name)_pen.cfg"
+    (Get-Content "$($vip.name)_pen.cfg") -replace "SRCPRT",$vip.Source | Set-Content "$($vip.name)_pen.cfg"
+    (Get-Content "$($vip.name)_pen.cfg") -replace "DESTIP",$vip.ip | Set-Content "$($vip.name)_pen.cfg"
+    (Get-Content "$($vip.name)_pen.cfg") -replace "DEST2IP",$vip.ip2 | Set-Content "$($vip.name)_pen.cfg"
+    (Get-Content "$($vip.name)_pen.cfg") -replace "DESTPORT",$vip.dport | Set-Content "$($vip.name)_pen.cfg"
+    
+    }
---
+++ zen_lb-if-generator.ps1
@@ -0,0 +1,26 @@
+﻿#This script creates the interface files required for new VIPs on ZEN Load Balancer
+#The script is just doing a find and replace 
+#You'll need to generate a csv file that has all the unique VIP IPs 
+#ips-template.csv is a quick template file to reference
+#I just pulled all the unique IPs from the environment config sheets and dump them into the template file
+#Once you have all the files generated you'll need to scp them over to the load balancer
+#once on the load balancer you'll need to rename all the files as the format of the file needs to be this
+#if_eth1:92_conf but Windows doesn't allow : in the file name. From the linux command line run
+#rename -v 's/\./\:/' * in the directory only containing the files you want to rename 
+#update the if-template_conf file for whatever eth# interface on the LB you are creating VIPs for
+#
+#
+#pthomson - May 26, 2015
+#
+#
+
+$csv = Read-host "Enter the name of the .CSV file located in the same folder as this script"
+$vips = Import-Csv $csv
+$a = 1
+
+foreach ($vip in $vips) {
+    (Get-Content .\if-template_conf) -replace "XX",$a | Set-Content "if_eth3.$($a)_conf"
+    (Get-Content "if_eth3.$($a)_conf") -replace "IP",$vip.vip | Set-Content "if_eth3.$($a)_conf"
+    
+    $a++
+}
